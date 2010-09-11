ec10d3e242777b26d5aa9a6b4a98403466512fce
plummer (Martyn Plummer) <plummer@iarc.fr>
2010-09-11 12:40:31 +0000

    Try to use latest version of OpenBUGS on Windows
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bugs-examples@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/windows/zzz.R
+++ pkg/R/windows/zzz.R
@@ -1,9 +1,23 @@
 .onLoad <- function(pkg, lib)
 {
     if (is.null(getOption("WinBUGSDir"))) {
-        options("WinBUGSDir" = "C:/Program Files/WinBUGS14")
+        if (file.exists("C:/Program Files/WinBUGS14")) {
+            options("WinBUGSDir" = "C:/Program Files/WinBUGS14")
+        }
     }
-    if (is.null(getOption("WinBUGSDir"))) {
-        options("OpenBUGSDir" = "C:/Program Files/OpenBUGS/OpenBUGS307")
+    if (is.null(getOption("OpenBUGSDir"))) {
+        ## Try to identify and use the latest OpenBUGS installation
+        base.path <- "C:/Program Files/OpenBUGS"
+        if (file.exists(base.path) && file.info(base.path)$isdir) {
+            ob <- list.files(base.path)
+            ob <- ob[tolower(substr(ob, 1, 8)) == "openbugs"]
+            ob.version <- substring(ob, from=9)
+            ob.version <- suppressWarnings(as.integer(ob))
+            if (!all(is.na(ob.version))) {
+                ob.latest <- ob[which.max(ob.version)]
+                options("OpenBUGSDir" = file.path(base.path, ob.latest))
+            }
+        }
     }
 }
+
